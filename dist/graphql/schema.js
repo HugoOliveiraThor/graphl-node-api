bead2d6ef82224240e4311b28cf668a473a512e5
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2019-03-03 09:32:04 -0300

    Create first mutation

--- dist/graphql/schema.js
+++ dist/graphql/schema.js
@@ -13,10 +13,17 @@ var users = [
         email: 'hugo@email.com'
     }
 ];
-var typeDefs = "\n  type User {\n    id:ID!\n    name:String!\n    email:String!\n  }\n\n  type Query {\n    allUsers: [User!]!\n  }\n";
+var typeDefs = "\n  type User {\n    id:ID!\n    name:String!\n    email:String!\n  }\n\n  type Query {\n    allUsers: [User!]!\n  }\n\n  type Mutation {\n    createUser(name:String!, email: String!): User\n  }\n";
 var resolvers = {
     Query: {
         allUsers: function () { return users; }
+    },
+    Mutation: {
+        createUser: function (parent, args) {
+            var newUser = Object.assign({ id: users.length + 1 }, args);
+            users.push(newUser);
+            return newUser;
+        }
     }
 };
 exports["default"] = graphql_tools_1.makeExecutableSchema({ typeDefs: typeDefs, resolvers: resolvers });
