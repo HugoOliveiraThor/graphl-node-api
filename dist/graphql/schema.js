74146e20c40c59571a11423d6dd906ffdbd6aede
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2019-03-03 09:23:42 -0300

    Implement first query in graphql and set the grafic interface

---
+++ dist/graphql/schema.js
@@ -0,0 +1,22 @@
+"use strict";
+exports.__esModule = true;
+var graphql_tools_1 = require("graphql-tools");
+var users = [
+    {
+        id: 1,
+        name: 'Jon',
+        email: 'jon@email.com'
+    },
+    {
+        id: 2,
+        name: 'Hugo',
+        email: 'hugo@email.com'
+    }
+];
+var typeDefs = "\n  type User {\n    id:ID!\n    name:String!\n    email:String!\n  }\n\n  type Query {\n    allUsers: [User!]!\n  }\n";
+var resolvers = {
+    Query: {
+        allUsers: function () { return users; }
+    }
+};
+exports["default"] = graphql_tools_1.makeExecutableSchema({ typeDefs: typeDefs, resolvers: resolvers });
